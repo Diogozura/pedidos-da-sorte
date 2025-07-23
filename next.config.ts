315109393b95ec3f5ca478dfc09df059fd3b275e
Diogo Zura <65250673+Diogozura@users.noreply.github.com>
2025-07-23 14:09:04 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['www.pedidosdasorte.com.br', 'sorteio.pedidodasorte.com.br'],
+  },
 };
 
 export default nextConfig;
