00934fb5bf8f989cc3c4e3d4a984f33b1abf1f0f
Diogo Zura <65250673+Diogozura@users.noreply.github.com>
2025-07-29 11:38:32 -0300

    hotfix: remove route para pedido

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['pedidodasorte.com.br', 'www.pedidodasorte.com.br']
-  },
+  
 };
 
 export default nextConfig;
