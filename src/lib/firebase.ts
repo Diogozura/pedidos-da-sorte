e5bc2bf34e8dfc9da9e72dc2f2098413d3ed121e
Diogo Zura <65250673+Diogozura@users.noreply.github.com>
2025-08-11 11:01:16 -0300

    hotfix : deslogar após fechar tela

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, setPersistence, browserSessionPersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth = getAuth(app); 
 export const db = getFirestore(app);
+setPersistence(auth, browserSessionPersistence).catch(console.error);
